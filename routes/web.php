390f68c1d19d4bab9a463c77a626c4e8f06596b8
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2022-07-02 21:54:35 +0800

    7/2/22

--- routes/web.php
+++ routes/web.php
@@ -3,20 +3,21 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 
+Route::group(['middleware' => 'prevent-back-history'], function () {
+    Route::get('/', function () {
+        return view('welcome');
+    });
 
-Route::get('/', function () {
-    return view('welcome');
-});
+    Route::middleware([
+        'auth:sanctum',
+        config('jetstream.auth_session'),
+        'verified'
+    ])->group(function () {
+        Route::get('/dashboard', function () {
+            return view('admin.index');
+        })->name('dashboard');
+    });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('admin.index');
-    })->name('dashboard');
-});
-
-// Logout Route
-Route::get('admin/logout', [AdminController::class, 'Logout'])->name('admin.logout');
+    // Logout Route
+    Route::get('admin/logout', [AdminController::class, 'Logout'])->name('admin.logout');
+}); // End Prevent Back Middleware 
