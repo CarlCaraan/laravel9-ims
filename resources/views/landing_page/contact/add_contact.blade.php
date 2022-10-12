c2ab782352f0f4d1073b6c4ef6e2329d72734fee
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2022-10-12 19:05:37 +0800

    10/12/22

--- resources/views/landing_page/contact/add_contact.blade.php
+++ resources/views/landing_page/contact/add_contact.blade.php
@@ -24,7 +24,7 @@
             <h2>Send us your Inquiries</h2>
         </div>
         <div class="row">
-            <div class="col-md-6">
+            <div class="col-md-8">
                 <div class="card shadow-sm border-0">
                     <div class="card-header bg-white">
                         Fill up information below, once we received wait atleast 3-4 business days
@@ -62,11 +62,20 @@
                     </div>
                 </div>
             </div>
-            <div class="col-md-6">
-                <iframe class="border" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1878.2638120317586!2d121.41797510357654!3d14.27640747501679!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397e314eff663e1%3A0xe0f99b67d115a8ee!2sDepartment%20of%20Education%20-%20Schools%20Division%20Office%20of%20Laguna!5e0!3m2!1sen!2sph!4v1664203063986!5m2!1sen!2sph" width="100%" height="100%" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <div class="col-md-4">
+                <img class="img-fluid" src="{{ asset('landing_page/assets/img/contact/contact_bg.svg') }}" alt="contact">
             </div>
         </div>
 
+
+        <div class="section-header mt-5">
+            <h2>Our Location</h2>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <iframe class="border" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1878.2638120317586!2d121.41797510357654!3d14.27640747501679!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397e314eff663e1%3A0xe0f99b67d115a8ee!2sDepartment%20of%20Education%20-%20Schools%20Division%20Office%20of%20Laguna!5e0!3m2!1sen!2sph!4v1664203063986!5m2!1sen!2sph" width="100%" height="500" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+        </div>
 
     </div>
 </section>
