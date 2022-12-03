5c9c8323fcd58c0883f9fad1e3118cec8133f557
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2022-12-03 13:06:12 +0800

    12/3/22

--- resources/views/landing_page/index.blade.php
+++ resources/views/landing_page/index.blade.php
@@ -30,8 +30,8 @@
         </div>
         <div class="row">
             <div class="col-lg-4 about-img">
-                <!-- <img src="{{ asset('landing_page/assets/img/about-img.jpg') }}" alt=""> -->
-                <img src="http://depedbenguet.com/wp-content/uploads/2017/03/SDS-grad-pics-300x300.png}" alt="sds_image">
+                <img src="{{ asset('landing_page/assets/img/about-img.png') }}" alt="message-from-sds">
+                <!-- <img src="http://depedbenguet.com/wp-content/uploads/2017/03/SDS-grad-pics-300x300.png" alt="sds_image"> -->
             </div>
 
             <div class="col-lg-8 content">
