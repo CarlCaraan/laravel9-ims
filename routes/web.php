f79365928c21e2a4b2ba36df5d5f32d501ad6dc0
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2022-12-22 10:18:56 +0800

    12/22/22

--- routes/web.php
+++ routes/web.php
@@ -54,6 +54,7 @@ Route::group(['middleware' => 'prevent-back-history'], function () {
     });
 
     // ========= ALL Admin Routes =========
+
     Route::middleware([
         'auth:sanctum',
         config('jetstream.auth_session'),
